18d13b11d7574e34ca8ec7ba0559809ae7333050
Squ1gglyZeroCool <squ1gglyzerocool@gmail.com>
2023-06-14 23:42:05 +1000

    FiX: Fixed: and i did it myself hmmf :()

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 
 export default function errorMiddleware(error: Error, req: Request, res: Response, next: NextFunction) {
-  if (error) {
-    if (res.headersSent) {
-      console.error(error);
-      return;
-    }
-    console.error(error);
-    res.status(500).send({ message: error.message });
+  if (!error) {
+    return;
   }
+  if (res.headersSent) {
+    console.error(error);
+    return;
+  }
+  console.error(error);
+  res.status(500).send({ message: error.message });
   return;
 }
--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,3 +1,7 @@
 export function isNumberBetween1And100(number: number): boolean {
   return number <= 100 && number > 0;
 }
+
+export function whatIsThis(input: unknown) {
+  return typeof input;
+}
